ecee794c11220be98c23dbaa0c5878db38d9644a
Piotr Garstka <35033672+PiotrGarstka@users.noreply.github.com>
2018-10-03 10:41:03 +0200

    Add files via upload

---
+++ src/GuessNumber_1.java
@@ -0,0 +1,37 @@
+package warsztaty_1;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessNumber_1 {
+
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+        int random = rand.nextInt(100);
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Podaj liczbę:");
+        int value = scan.nextInt();
+
+        while (true) {
+                if (value > random) {
+                System.out.println("Za dużo!");
+                value=scan.nextInt();
+                }
+
+                if (value < random) {
+                System.out.println("Za mało!");
+                value=scan.nextInt();
+                }
+
+                if (value==random){
+                System.out.println("Zgadłeś!");
+                break;
+                }
+            }
+        }
+
+    }
+
+
